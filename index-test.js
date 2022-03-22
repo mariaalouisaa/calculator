942fd98a63a969869bc824902195e3492baf4820
= <maria.louisa.castro@gmail.com>
2022-03-22 15:39:50 +0000

    Add and link test files

---
+++ index-test.js
@@ -0,0 +1,43 @@
+// test("+ case returns correct result", () => {
+//   const actual = calculate(3, "+", 2);
+//   const expected = 5;
+//   equal(actual, expected);
+// });
+
+// test("- case returns correct result", () => {
+//   const actual = calculate(3, "-", 2);
+//   const expected = 1;
+//   equal(actual, expected);
+// });
+
+// test("/ case returns correct result", () => {
+//   const actual = calculate(6, "/", 2);
+//   const expected = 3;
+//   equal(actual, expected);
+// });
+
+// test("* case returns correct result", () => {
+//   const actual = calculate(6, "*", 2);
+//   const expected = 12;
+//   equal(actual, expected);
+// });
+
+// test("string entered instead of number", () => {
+//   const actual = calculate("6", "*", "2");
+//   const expected = 12;
+//   equal(actual, expected);
+// });
+
+// test("form submits expected result", () => {
+//   const inputa = document.querySelector("#a");
+//   inputa.value = 10;
+//   const inputb = document.querySelector("#b");
+//   inputb.value = 2;
+//   const inputsign = document.querySelector("#sign");
+//   inputsign.value = "*";
+//   const submitButton = document.querySelector("button[type='submit']");
+//   submitButton.click();
+//   const result = document.querySelector("#result");
+//   equal(result.textContent, "20");
+//   result.textContent = "";
+// });
